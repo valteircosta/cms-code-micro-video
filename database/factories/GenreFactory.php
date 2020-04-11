cc8af5a8935df3a7633de5a8fd39774e4cff57d9
valteir costa de sousa <valteircosta@gmail.com>
2020-04-10 22:33:12 -0300

    Add crud genre

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Genre::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->colorName,
+        'description' => rand(1, 10) % 2 == 0 ? $faker->sentence() : null
     ];
 });
