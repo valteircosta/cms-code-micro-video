b517140f25f579f1496377d2c50882711d8fee82
valteir costa de sousa <valteircosta@gmail.com>
2020-08-31 07:48:29 -0300

    Add GenreSeeder adpater para categories

--- database/seeds/GenreSeeder.php
+++ database/seeds/GenreSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class GenreSeeder extends Seeder
@@ -12,6 +13,12 @@ class GenreSeeder extends Seeder
     public function run()
     {
         //
-        factory(\App\Models\Genre::class, 20)->create();
+        $categories = Category::all();
+        factory(\App\Models\Genre::class, 20)
+            ->create()
+            ->each(function (Genre $genre) use ($categories) {
+                $categoriesId = $categories->random(5)->pluck('id')->toArray();
+                $genre->$categories()->attach($categoriesId);
+            });
     }
 }
