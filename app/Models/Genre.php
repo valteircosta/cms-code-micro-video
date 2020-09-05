0d44fb8dd2342f907f7de2edac9b2de7b0e0217e
valteir costa de sousa <valteircosta@gmail.com>
2020-09-04 22:24:12 -0300

    Add ide-helpre

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,25 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
- * App\Models\Genre
- *
- * @property string $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre newQuery()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre query()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Genre whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Genre withoutTrashed()
- * @mixin \Eloquent
- */
 class Genre extends Model
 {
     use SoftDeletes, Traits\Uuid;
