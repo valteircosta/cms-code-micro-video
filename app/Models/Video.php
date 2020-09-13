538fe0304c4bd7cca70bed21c39a24de58914d5a
valteir costa de sousa <valteircosta@gmail.com>
2020-09-13 19:44:21 +0000

    Add override method create and update  Video

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -32,6 +34,50 @@ class Video extends Model
 
     public $incrementing = false;
 
+    //First is called magic method what call create, it not exist then called
+    // QueryBuilder constructor
+    public static  function create(array $attributes = [])
+    {
+        try {
+            //Disable auto commit
+            \DB::beginTransaction();
+            $obj = static::query()->create($attributes);
+            // Do upload here
+            \DB::commit();
+            return $obj;
+        } catch (\Exception $e) {
+            if (isset($obj)) {
+                // Do delete uploaded files
+            }
+            \DB::rollBack();
+            throw $e; // Rise execption for laravel
+        }
+    }
+    /**
+     * Override method update Model
+     * @param array $attributes
+     * @param array $options
+     * @return bool
+     * @throws Exception
+     */
+    public function update(array $attributes = [], array $options = [])
+    {
+        try {
+            //Disable auto commit
+            \DB::beginTransaction();
+            $saved = parent::update($attributes, $options);
+            if ($saved) {
+                //Do upload new file here
+                //Do delete older file
+            }
+            \DB::commit();
+            return $saved;
+        } catch (\Exception $e) {
+            //Do delete new files try update here
+            \DB::rollBack();
+            throw $e; // Rise execption for laravel
+        }
+    }
     public function categories()
     {
         //with trashed traz a categorias antigas que já foram excluidas
