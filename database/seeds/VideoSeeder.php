8bb19ed36b7f7c2fbd6463c051563adadf5600c3
valteir costa de sousa <valteircosta@gmail.com>
2020-10-12 11:49:14 +0000

    Add method fetchRelations

--- database/seeds/VideoSeeder.php
+++ database/seeds/VideoSeeder.php
@@ -2,10 +2,17 @@
 
 use App\Models\Genre;
 use App\Models\Video;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class VideoSeeder extends Seeder
 {
+    private $allGenres;
+    private $relations = [
+        'genres_id' => [],
+        'categories_id' => [],
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -13,20 +20,29 @@ class VideoSeeder extends Seeder
      */
     public function run()
     {
-        $genres = Genre::all();
+        $dir = \Storage::getDriver()->getAdapter()->getPathPrefix();
+        \File::deleteDirectory($dir, true);
+
+        $self = $this;
+        $this->allgenres = Genre::all();
+        Model::reguard(); //Active mass assignament in the model
         factory(\App\Models\Video::class, 100)
-            ->create()
+            ->make()
             ->each(function (Video $video) use ($genres) {
-                $subGenres = $genres->random(5)->load('categories');
-                $categoriesId = [];
-                //Usando operador spread PHP ... p/ obter [1,2,3,4,5]
-                foreach ($subGenres as $genre) {
-                    array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
-                }
-                $categoriesId = array_unique($categoriesId);
-                $video->categories()->attach($categoriesId);
-                $subGenresId = $subGenres->pluck('id')->toArray();
-                $video->genres()->attach($subGenresId);
             });
+        Model::unguard(); //Return false mass assignament
+    }
+    public function fetchRelations()
+    {
+        $subGenres = $this->allGenres->random(5)->load('categories');
+        $categoriesId = [];
+        //Using operator spread PHP ... for get [1,2,3,4,5]
+        foreach ($subGenres as $genre) {
+            array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
+        }
+        $categoriesId = array_unique($categoriesId);
+        $subGenresId = $subGenres->pluck('id')->toArray();
+        $this->relations['categories_id'] = $categoriesId;
+        $this->relations['genres_id'] = $subGenresId;
     }
 }
