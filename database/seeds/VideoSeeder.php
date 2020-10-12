213204d476463f7ce8248b0782909c553c4a73d5
valteir costa de sousa <valteircosta@gmail.com>
2020-10-12 14:49:42 +0000

    Rename files models para base-image and base-video

--- database/seeds/VideoSeeder.php
+++ database/seeds/VideoSeeder.php
@@ -4,6 +4,7 @@ use App\Models\Genre;
 use App\Models\Video;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 
 class VideoSeeder extends Seeder
 {
@@ -24,14 +25,28 @@ class VideoSeeder extends Seeder
         \File::deleteDirectory($dir, true);
 
         $self = $this;
-        $this->allgenres = Genre::all();
-        Model::reguard(); //Active mass assignament in the model
-        factory(\App\Models\Video::class, 100)
+        $this->allGenres = Genre::all();
+        Model::reguard(); //Active mass assignment in the model
+        factory(\App\Models\Video::class, 50)
             ->make()
-            ->each(function (Video $video) use ($genres) {
+            ->each(function (Video $video) use ($self) {
+                $self->fetchRelations();
+                \App\Models\Video::create(
+                    array_merge(
+                        $video->toArray(),
+                        [
+                            'thumb_file' => $self->getImageFile(),
+                            'banner_file' => $self->getImageFile(),
+                            'video_file' => $self->getVideoFile(),
+                            'trailer_file' => $self->getVideoFile(),
+                        ],
+                        $this->relations
+                    )
+                );
             });
-        Model::unguard(); //Return false mass assignament
+        Model::unguard(); //Return to false mass assignment
     }
+
     public function fetchRelations()
     {
         $subGenres = $this->allGenres->random(5)->load('categories');
@@ -44,5 +59,21 @@ class VideoSeeder extends Seeder
         $subGenresId = $subGenres->pluck('id')->toArray();
         $this->relations['categories_id'] = $categoriesId;
         $this->relations['genres_id'] = $subGenresId;
+    }
+
+    public function getImageFile()
+    {
+        return new UploadedFile(
+            storage_path('faker/thumbs/base-image.png'),
+            '01-how-functionate-upload-image.png'
+        );
+    }
+
+    public function getVideoFile()
+    {
+        return new UploadedFile(
+            storage_path('faker/videos/base-video.mp4'),
+            '01-how-functionate-upload-video.mp4'
+        );
     }
 }
