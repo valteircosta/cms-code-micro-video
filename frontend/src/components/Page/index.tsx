b561ceefcbed749b8b482e3c04224437163c794b
valteir costa de sousa <valteircosta@gmail.com>
2020-10-31 19:05:44 +0000

    Add title

--- frontend/src/components/Page/index.tsx
+++ frontend/src/components/Page/index.tsx
@@ -1,14 +1,20 @@
 // @flow 
-import { Container, Typography } from '@material-ui/core';
+import { Container, makeStyles, Typography } from '@material-ui/core';
 import * as React from 'react';
+
+const useStyles = makeStyles({
+    title: { color='#999999' }
+});
+
 type PageProps = {
     title: String;
 };
 export const Page: React.FC<PageProps> = (props) => {
+    const classes = useStyles();
     return (
         <div>
             <Container>
-                <Typography>
+                <Typography className={classes.title} >
                     {props.title}
                 </Typography>
             </Container>
