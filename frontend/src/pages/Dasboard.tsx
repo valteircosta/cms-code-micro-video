55f57284ac9a1e57c1e1a30befb713420f682fb9
valteir costa de sousa <valteircosta@gmail.com>
2020-10-31 22:22:35 +0000

    Add page Dashboard

---
+++ frontend/src/pages/Dasboard.tsx
@@ -0,0 +1,13 @@
+// @flow 
+import * as React from 'react';
+
+interface DashboardProps {
+
+};
+export const Dasboard = (props: DashboardProps) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
