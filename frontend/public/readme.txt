a97db723129f255d06567692fa7e715bdd66c140
valteir costa de sousa <valteircosta@gmail.com>
2020-11-01 13:48:29 +0000

    Added property name  in MyRouteProps interface

--- frontend/public/readme.txt
+++ frontend/public/readme.txt
@@ -1,3 +1,5 @@
+git log --pretty=format:'%C(blue)%h %C(red)%d %C(white)%s -%C(cyan) %cn,%C(green) %cr'                                                   Sun 01 Nov 2020 12:35:52 PM UTC
+
 File with information development class
 
 Video stop moment : 7:00 Integrando material-ui with front end
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,18 +4,21 @@ import CategoryList from '../pages/category/List';
 
 // Declaring new interface that extended RouteProps
 interface MyRouteProps extends RouteProps {
+    name: string;
     label: string;
 }
 
 const routes: MyRouteProps[] = [
     {
         label: 'Dashboard',
+        name: 'dashboard',
         path: '/',
         component: Dashboard,
         exact: true
     },
     {
         label: 'List of categories',
+        name: 'categories.list',
         path: '/categories',
         component: CategoryList,
         exact: true
