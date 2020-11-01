b615dee7f4663a264b5fe2aa54b307ed8841c17f
valteir costa de sousa <valteircosta@gmail.com>
2020-11-01 00:11:36 +0000

    Added routes in separate file as better programmation  practice

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { RouteProps } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import CategoryList from '../pages/category/List';
 
 // Declaring new interface that extended RouteProps
 interface MyRouteProps extends RouteProps {
@@ -9,13 +11,13 @@ const routes: MyRouteProps[] = [
     {
         label: 'Dashboard',
         path: '/',
-        component: '',
+        component: Dashboard,
         exact: true
     },
     {
-        label: 'Lista de categorias',
+        label: 'Listagem de categorias',
         path: '/categories',
-        component: '',
+        component: CategoryList,
         exact: true
     }
 ];
