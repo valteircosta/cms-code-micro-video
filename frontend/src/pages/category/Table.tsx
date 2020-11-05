1603f749fc6e12cb6167a3b356cb7f573664dce7
valteir costa de sousa <valteircosta@gmail.com>
2020-11-05 11:04:08 +0000

    Added mui-datatables example

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,13 +1,43 @@
 // @flow 
 import * as React from 'react';
+import MUIDataTable, { MUIDataTableColumn } from 'mui-datatables';
+
+const columnsDefinitions: MUIDataTableColumn[] = [
+    {
+        name: 'name',
+        label: 'Nome'
+    },
+    {
+        name: 'is_active',
+        label: 'Ativo?'
+    },
+    {
+        name: 'created_at',
+        label: 'Criado em'
+    },
+];
+
+const data = [
+    { name: 'title', is_active: true, created_at: '2020-10-01' },
+    { name: 'title1', is_active: false, created_at: '2020-10-02' },
+    { name: 'title2', is_active: true, created_at: '2020-10-03' },
+    { name: 'title3', is_active: false, created_at: '2020-10-04' },
+    { name: 'title4', is_active: true, created_at: '2020-10-05' },
+    { name: 'title5', is_active: false, created_at: '2020-10-06' },
+    { name: 'title6', is_active: true, created_at: '2020-10-07' },
+];
+
 type Props = {
 
 };
+
 const Table = (props: Props) => {
     return (
-        <div>
-            Table
-        </div>
+        <MUIDataTable
+            title='Listagem de categorias'
+            data={data}
+            columns={columnsDefinitions}
+        />
     );
 };
 export default Table;
