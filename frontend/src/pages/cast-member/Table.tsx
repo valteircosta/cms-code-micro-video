f1f63fea9d471f485a1a914dc4e004596176ad1c
valteir costa de sousa <valteircosta@gmail.com>
2020-11-08 23:58:33 +0000

    Added lists

---
+++ frontend/src/pages/cast-member/Table.tsx
@@ -0,0 +1,68 @@
+// @flow 
+import * as React from 'react';
+import MUIDataTable, { MUIDataTableColumn } from 'mui-datatables';
+import { useState, useEffect } from 'react';
+import { httpVideo } from '../../util/http';
+import format from 'date-fns/format';
+import parseISO from 'date-fns/parseISO';
+
+// With noImplicintAny = true must declare type
+// const CastMemberTypeMap: { [key: number]: string } = {
+//     1: "Diretor",
+//     2: "Ator",
+// };
+
+const CastMemberTypeMap= {
+    1: "Diretor",
+    2: "Ator",
+};
+
+const columnsDefinitions: MUIDataTableColumn[] = [
+    {
+        name: 'name',
+        label: 'Nome'
+    },
+    {
+        name: 'type',
+        label: 'Tipo',
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return CastMemberTypeMap[value];
+            }
+        }
+    },
+    {
+        name: 'created_at',
+        label: 'Criado em',
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return <span>{format(parseISO(value), 'dd/MM/yyyy')}</span>;
+            }
+        }
+    },
+];
+
+
+type Props = {
+
+};
+
+const Table = (props: Props) => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        httpVideo.get('cast_members').then(
+            (response) => setData(response.data.data)
+        )
+    }, []);
+
+    return (
+        <MUIDataTable
+            title='Listagem de categorias'
+            data={data}
+            columns={columnsDefinitions}
+        />
+    );
+};
+export default Table;
