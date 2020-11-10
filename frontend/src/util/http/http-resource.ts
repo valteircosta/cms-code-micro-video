4d545bb92a36eba733f3f9943da152b896b490eb
valteir costa de sousa <valteircosta@gmail.com>
2020-11-10 10:59:04 +0000

    Finished component http-resource with promise and type generic

--- frontend/src/util/http/http-resource.ts
+++ frontend/src/util/http/http-resource.ts
@@ -1,30 +1,26 @@
-import { AxiosInstance } from "axios";
+import { AxiosInstance, AxiosResponse } from "axios";
+import { AnySoaRecord } from "dns";
 
+export default class HttpResource {
+  constructor(protected http: AxiosInstance, protected resource) {}
 
- export default class HttpResource {
-    
-    constructor(protected http: AxiosInstance, protected resource) {
-        
-    }
+  list<T = any>(): Promise<AxiosResponse<T>> {
+    return this.http.get<T>(this.resource);
+  }
 
-    list<T = any >() {
-        return this.http.get<T> (this.resource);
-    }
+  get<T = any>(id: any): Promise<AxiosResponse<T>> {
+    return this.http.get<T>(`${this.resource}/{$id}`);
+  }
 
-    get() {
-        
+  create<T = any>(data: any): Promise<AxiosResponse<T>> {
+    return this.http.post<T>(this.resource, data);
+  }
 
-    }
-    
-    create() {
-        
-    }
-    
-    update() {
-        
-    }
-    
-    delete() {
-    
-    }
+  update<T = any>(id: any, data: any): Promise<AxiosResponse<T>> {
+    return this.http.put<T>(`${this.resource}/${id}`, data);
+  }
+
+  delete<T = any>(id: AnySoaRecord) {
+    return this.http.delete<T>(`${this.resource}/${id}`);
+  }
 }
