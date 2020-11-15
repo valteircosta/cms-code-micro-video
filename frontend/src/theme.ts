7bdf33f2f7d856a4c73b615bf0347adaa6d3ee9a
valteir costa de sousa <valteircosta@gmail.com>
2020-11-15 10:45:47 -0300

    Added theme.ts and configuring container frontend

---
+++ frontend/src/theme.ts
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core";
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#79aec8',
+            contrastText: '#ffff'
+        },   
+        secondary: {
+            main: '#4db5ab',
+            contrastText: '#ffff'
+        }   
+ }
+});
+
+export default theme;
