6222fe7696096166db17f06af59205c6d3457f76
valteir costa de sousa <valteircosta@gmail.com>
2020-11-16 08:15:14 -0300

    Added file material-ui.d.ts with sample as override type in Typescript

--- frontend/src/typings/material-ui.d.ts
+++ frontend/src/typings/material-ui.d.ts
@@ -3,6 +3,7 @@
 /* cSpell:enable */
 
 import { ComponentNameToClassKey } from "@material-ui/core/styles/overrides";
+
 declare module "@material-ui/core/styles/overrides" {
   interface ComponentNameToClassKey {
     MUIDataTable: any;
