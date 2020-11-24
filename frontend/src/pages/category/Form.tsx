7b2214f8da127b8d729163033ca419dd23484f53
valteir costa de sousa <valteircosta@gmail.com>
2020-11-24 12:10:09 +0000

    Added validation  rules name field

--- frontend/src/pages/category/Form.tsx
+++ frontend/src/pages/category/Form.tsx
@@ -2,7 +2,9 @@
 import { Box, Button, ButtonProps, Checkbox, makeStyles, TextField, Theme } from '@material-ui/core';
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import categoryHttp from '../../util/http/category-http';
+import * as yup from 'yup';
 
 const useStyles = makeStyles((theme: Theme) => {
     return {
@@ -12,6 +14,11 @@ const useStyles = makeStyles((theme: Theme) => {
     }
 });
 
+const validationSchema = yup.object().shape({
+    name: yup.string()
+        .label('Nome')
+        .required(),
+});
 export const Form = () => {
 
     const classes = useStyles();
@@ -25,8 +32,10 @@ export const Form = () => {
     //Using component react-hook-form 
     const { register, handleSubmit, getValues, errors } = useForm({
         defaultValues: {
+            name: null,
             is_active: true
-        }
+        },
+        resolver: yupResolver(validationSchema),
     });
 
     function onSubmit(formData, event) {
@@ -34,7 +43,6 @@ export const Form = () => {
             .create(formData)
             .then((response) => console.log(response))
     }
-    console.log(errors);
     return (
         <form onSubmit={handleSubmit(onSubmit)} >
             <TextField
@@ -44,8 +52,11 @@ export const Form = () => {
                 variant='outlined'
                 margin='normal'
                 inputRef={register({
-                    required: 'O campo nome é requerido'
+                    required: 'O campo nome é requerido',
                 })}
+
+                error={errors.name !== undefined}
+                helperText={errors.name && errors.name.message}
             />
             <TextField
                 name='description'
