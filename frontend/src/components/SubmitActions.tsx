87bbec6e789985abd968e09b94bb02e4cf1d2db3
valteir costa de sousa <valteircosta@gmail.com>
2020-12-04 02:40:24 +0000

    Added   new component  SubmitActions

---
+++ frontend/src/components/SubmitActions.tsx
@@ -0,0 +1,41 @@
+// @flow
+import * as React from 'react';
+import { Box, Button, makeStyles, Theme } from '@material-ui/core';
+import { ButtonProps } from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) => {
+    return {
+        submit: {
+            margin: theme.spacing(1)
+        }
+    }
+});
+interface SubmitActionsProps {
+    disableButtons?: boolean;
+    handleSave: () => void;
+}
+
+const SubmitActions: React.FC<SubmitActionsProps> = (props) => {
+
+    const classes = useStyles();
+    const buttonProps: ButtonProps = {
+        className: classes.submit,
+        color: 'secondary',
+        variant: 'contained',
+        disabled: props.disableButtons === undefined ? false : props.disableButtons
+    }
+
+
+    return (
+        <Box dir={'rtl'} >
+            <Button
+                color={'primary'}
+                {...buttonProps}
+                onClick={props.handleSave}
+            >Salvar
+            </Button>
+            <Button {...buttonProps} type='submit' >Salvar e continuar editando</Button>
+        </Box>
+    );
+};
+export default SubmitActions;
