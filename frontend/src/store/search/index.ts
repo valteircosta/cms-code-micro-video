b0785b2efad4b0216b44ee547bd807d5e3ffde4f
valteir costa de sousa <valteircosta@gmail.com>
2020-12-14 11:38:15 +0000

    Introduce react suace made

--- frontend/src/store/search/index.ts
+++ frontend/src/store/search/index.ts
@@ -1,3 +1,12 @@
-import { SetSearchAction } from './types';
-import { createrAction } from 'reduxsauce';
-
+import { SetSearchAction } from "./types";
+import { createActions } from "reduxsauce";
+/**
+ * Using reduxsauce will make types and constant action name 'SET_SEARCH']
+ * Not will need  name type 'SetSearchAction' complete also the payload is added automatic
+ */
+const { Types, Creators } = createActions({
+  setSearch: ["payload"],
+  setPage: ["payload"],
+  setPerPage: ["payload"],
+  SetSortOrder: ["payload"],
+});
