bcf9ddf59721ec13c26b51642874285760cf6efa
valteir costa de sousa <valteircosta@gmail.com>
2020-12-14 12:05:54 +0000

    Make  types for help code in the IDE

--- frontend/src/store/search/index.ts
+++ frontend/src/store/search/index.ts
@@ -1,10 +1,29 @@
-import { SetSearchAction } from "./types";
+import * as Typings from "./types";
 import { createActions } from "reduxsauce";
 /**
  * Using reduxsauce will make types and constant action name 'SET_SEARCH']
  * Not will need  name type 'SetSearchAction' complete also the payload is added automatic
  */
-const { Types, Creators } = createActions({
+const { Types, Creators } = createActions<
+  {
+    SET_SEARCH: string;
+    SET_PAGE: string;
+    SET_PER_PAGE: string;
+    SET_SORT_ORDER: string;
+  },
+  {
+    setSearch(
+      payload: Typings.SetSearchAction["payload"]
+    ): Typings.SetSearchAction;
+    setPage(payload: Typings.SetPageAction["payload"]): Typings.SetPageAction;
+    setPerPage(
+      payload: Typings.SetPerPageAction["payload"]
+    ): Typings.SetPerPageAction;
+    setSortOrder(
+      payload: Typings.SetSortOrderAction["payload"]
+    ): Typings.SetSortOrderAction;
+  }
+>({
   setSearch: ["payload"],
   setPage: ["payload"],
   setPerPage: ["payload"],
