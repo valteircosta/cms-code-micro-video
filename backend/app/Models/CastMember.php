2c96c9d9cc69bd7885b30bd9f6770bdc3c34a484
valteir costa de sousa <valteircosta@gmail.com>
2020-12-26 22:07:04 +0000

    Added filter in Genre and CastMember

--- backend/app/Models/CastMember.php
+++ backend/app/Models/CastMember.php
@@ -1,19 +1,31 @@
 <?php
 
 namespace App\Models;
-
+use App\ModelFilters\CastMemberFilter;
+use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CastMember extends Model
 {
-    use SoftDeletes, Traits\Uuid;
-    //Contantes do tipo
+    use SoftDeletes, Traits\Uuid,Filterable ;
+
     const TYPE_DIRECTOR = 1;
     const TYPE_ACTOR = 2;
+
+    public static $types = [
+        CastMember::TYPE_DIRECTOR,
+        CastMember::TYPE_ACTOR,
+    ];
 
     protected $fillable = ['name', 'type'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string', 'name' => 'string', 'type' => 'integer'];
     public $incrementing = false;
+
+    //Do filter in class using Filter
+    public function modelFilter()
+    {
+        return $this->provideFilter(CastMemberFilter::class);
+    }
 }
