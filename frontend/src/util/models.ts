678301bd72677ba637c769ab3fedf5857d088c5a
valteir costa de sousa <valteircosta@gmail.com>
2021-04-25 22:46:55 +0000

    Create interface Video

--- frontend/src/util/models.ts
+++ frontend/src/util/models.ts
@@ -34,12 +34,37 @@ export interface CastMember extends Timestampable {
   type: number;
 }
 export const CastMemberTypeMap = {
-    1: "Diretor",
-    2: "Ator",
+  1: "Diretor",
+  2: "Ator",
 };
 export interface Genre extends Timestampable {
   readonly id: string;
   name: string;
   is_active: boolean;
   categories: Category[];
+}
+
+interface GenreVideo extends Omit<Genre, "categories"> {}
+
+export const VideoFileFieldsMap = {
+  thumb_file: "Thumbnail",
+  banner_file: "Banner",
+  trailer_file: "Trailer",
+  video_file: "Principal",
+};
+
+export interface Video extends Timestampable {
+  readonly id: string;
+  title: string;
+  description: string;
+  year_launched: number;
+  opened: boolean;
+  rating: string;
+  duration: number;
+  genres: GenreVideo[];
+  categories: Category[];
+  thumb_file_url: string;
+  banner_file_url: string;
+  trailer_file_url: string;
+  video_file_url: string;
 }
